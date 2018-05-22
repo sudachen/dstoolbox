3cca212a09f94e7e12e8bfccd60eec34808d9d7c
Alexey Sudachen <alexey@sudachen.name>
2018-05-22 00:02:22 -0400

    updated

---
+++ toolbox/jupyter_notebook_config.py
@@ -0,0 +1,19 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+from jupyter_core.paths import jupyter_data_dir
+import subprocess
+import os
+import errno
+import stat
+
+c = get_config()
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = 8888
+c.NotebookApp.open_browser = False
+c.NotebookApp.token = ''
+c.FileContentsManager.delete_to_trash = False
+c.NotebookApp.notebook_dir = '/home/jupyter/work'
+
+if os.environ.get('GITHUB_ACCESS_TOKEN','NONE') != 'NONE':
+    c.GitHubConfig.access_token = os.environ['GITHUB_ACCESS_TOKEN']
---
+++ toolbox/load_ext.py
@@ -0,0 +1,13 @@
+from IPython import get_ipython
+ip = get_ipython()
+
+ip.magic('load_ext sql')
+ip.magic('load_ext autoreload')
+ip.magic('matplotlib inline')
+ip.magic('config SqlMagic.autopandas = True')
+
+ip.run_cell('import matplotlib.pyplot as plt')
+ip.run_cell('import numpy as np')
+ip.run_cell('import pandas as pd')
+
+
