ff29a1b388640197e3f1a6c792cb210b9d777f92
Alexey Sudachen <alexey@sudachen.name>
2018-05-30 06:10:16 -0400

    dstoolbox-setup can update now

--- dstoolbox-setup.sh
+++ dstoolbox-setup.sh
@@ -11,8 +11,15 @@ DSTOOLBOX_KSETUP=${CONFIG_DIR}/setup_kernel.py
 GITHUB_TOOLBOX=https://raw.githubusercontent.com/sudachen/dstoolbox/master/toolbox/
 
 if [ -f ${CONFIG_DIR}/.version ]; then
-  echo "${CONFIG_DIR} already exists"
-  echo "run instead> sh ~/.dstoolbox/up"
+  echo "${CONFIG_DIR} updating ..."
+  curl -s ${GITHUB_TOOLBOX}/Dockerfile.in -o Dockerfile.in && \
+  curl -s ${GITHUB_TOOLBOX}/jupyter_notebook_config.py -o jupyter_notebook_config.py && \
+  curl -s ${GITHUB_TOOLBOX}/setup_kernel.py -o setup_kernel.py && \
+  curl -s ${GITHUB_TOOLBOX}/dst_up -o up && \
+  curl -s ${GITHUB_TOOLBOX}/dst_down -o down && \
+  chmod u+x up down && \
+  sh ./up && \
+  echo "success" || echo "failed"
 else
   mkdir -p ${CONFIG_DIR} && \
   cd ${CONFIG_DIR} && \
