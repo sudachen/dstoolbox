d19e664046fc1af754a8f2c07d6c696233d26978
Alexey Sudachen <alexey@sudachen.name>
2018-05-30 06:52:08 -0400

    dstoolbox-setup updated

--- dstoolbox-setup.sh
+++ dstoolbox-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 VERSION=1.6
+IMAGE=sudachen/jupy2r:latest
 
 CONFIG_DIR=${HOME}/.dstoolbox
 DSTOOLBOX_YAML=${CONFIG_DIR}/docker-compose.yml
@@ -10,12 +11,11 @@ DSTOOLBOX_NBCONFIG=${CONFIG_DIR}/jupyter_notebook_config.py
 DSTOOLBOX_KSETUP=${CONFIG_DIR}/setup_kernel.py
 GITHUB_TOOLBOX=https://raw.githubusercontent.com/sudachen/dstoolbox/master/toolbox/
 
-docker pull sudachen/jupy2r:latest
-
 if [ -f ${CONFIG_DIR}/.version ]; then
   if [ $(cat ${CONFIG_DIR}/.version) != ${VERSION} ]; then
     echo "updating at ${CONFIG_DIR} ..."
     cd ${CONFIG_DIR} && \
+    docker pull ${IMAGE} && \
     curl -s ${GITHUB_TOOLBOX}/Dockerfile.in -o Dockerfile.in && \
     curl -s ${GITHUB_TOOLBOX}/jupyter_notebook_config.py -o jupyter_notebook_config.py && \
     curl -s ${GITHUB_TOOLBOX}/setup_kernel.py -o setup_kernel.py && \
@@ -33,6 +33,7 @@ else
   echo "installing into ${CONFIG_DIR} ..."
   mkdir -p ${CONFIG_DIR} && \
   cd ${CONFIG_DIR} && \
+  docker pull ${IMAGE} && \
   curl -s ${GITHUB_TOOLBOX}/docker-compose.in.yml -o docker-compose.yml && \
   curl -s ${GITHUB_TOOLBOX}/Dockerfile.in -o Dockerfile.in && \
   curl -s ${GITHUB_TOOLBOX}/jupyter_notebook_config.py -o jupyter_notebook_config.py && \
