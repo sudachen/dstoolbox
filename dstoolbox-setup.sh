8065cbdc500f90f1e767508c2ea53a1970960190
Alexey Sudachen <alexey@sudachen.name>
2018-05-30 06:17:29 -0400

    dstoolbox-setup updated

--- dstoolbox-setup.sh
+++ dstoolbox-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=1.5
+VERSION=1.6
 
 CONFIG_DIR=${HOME}/.dstoolbox
 DSTOOLBOX_YAML=${CONFIG_DIR}/docker-compose.yml
@@ -11,16 +11,23 @@ DSTOOLBOX_KSETUP=${CONFIG_DIR}/setup_kernel.py
 GITHUB_TOOLBOX=https://raw.githubusercontent.com/sudachen/dstoolbox/master/toolbox/
 
 if [ -f ${CONFIG_DIR}/.version ]; then
-  echo "${CONFIG_DIR} updating ..."
-  curl -s ${GITHUB_TOOLBOX}/Dockerfile.in -o Dockerfile.in && \
-  curl -s ${GITHUB_TOOLBOX}/jupyter_notebook_config.py -o jupyter_notebook_config.py && \
-  curl -s ${GITHUB_TOOLBOX}/setup_kernel.py -o setup_kernel.py && \
-  curl -s ${GITHUB_TOOLBOX}/dst_up -o up && \
-  curl -s ${GITHUB_TOOLBOX}/dst_down -o down && \
-  chmod u+x up down && \
-  sh ./up && \
-  echo "success" || echo "failed"
+  if [ $(cat ${CONFIG_DIR}/.version) != ${VERSION} ]; then
+    echo "updating at ${CONFIG_DIR} ${CONFIG_DIR} ..."
+    curl -s ${GITHUB_TOOLBOX}/Dockerfile.in -o Dockerfile.in && \
+    curl -s ${GITHUB_TOOLBOX}/jupyter_notebook_config.py -o jupyter_notebook_config.py && \
+    curl -s ${GITHUB_TOOLBOX}/setup_kernel.py -o setup_kernel.py && \
+    curl -s ${GITHUB_TOOLBOX}/dst_up -o up && \
+    curl -s ${GITHUB_TOOLBOX}/dst_down -o down && \
+    chmod u+x up down && \
+    sh ./up && \
+    echo "${VERSION}" > .version &&\
+    echo "SUCCESS" || echo "FAIL"
+  else
+    echo "already installed"
+    echo "run ~/.dstoolbox/up instead"
+  fi
 else
+  echo "installing into ${CONFIG_DIR} ..."
   mkdir -p ${CONFIG_DIR} && \
   cd ${CONFIG_DIR} && \
   curl -s ${GITHUB_TOOLBOX}/docker-compose.in.yml -o docker-compose.yml && \
@@ -32,5 +39,6 @@ else
   curl -s ${GITHUB_TOOLBOX}/dst_down -o down && \
   chmod u+x up down && \
   sh ./up && \
-  echo "${VERSION}" > .version
+  echo "${VERSION}" > .version && \
+  echo "SUCCESS" || echo "FAIL"
 fi
